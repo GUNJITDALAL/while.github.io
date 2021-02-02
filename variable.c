04b68869b010b353bcc1e110986aac3ec46b47f7
gunjit <73329409+GUNJITDALAL@users.noreply.github.com>
2021-02-02 23:13:43 +0530

    Add files via upload

---
+++ variable.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int iname = 10.2;
+    float fname = 10.2;
+    double dnum = 10.2;
+    long lnum = 10.2;
+    char ch = 'a';
+    
+    printf("int =%d,float=%f,double=%lf,long=%ld,char=%c",iname,fname,dnum,lnum,ch);
+
+    return 0;
+}
